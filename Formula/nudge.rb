9ba2cb1774e5c28fdae1b4486b3cd930774d91db
darmiel <71837281+darmiel@users.noreply.github.com>
2024-05-27 00:39:13 +0200

    chore: updated sha256 for nudge

--- Formula/nudge.rb
+++ Formula/nudge.rb
@@ -2,7 +2,7 @@ class Nudge < Formula
   desc "CLI Tool to send files Peer-to-Peer using Hole Punch"
   homepage "https://github.com/darmiel/nudge"
   url "https://github.com/darmiel/nudge/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "97dd7f8e803e91aafe1eef59de1a43498731ab0f234199b7631923d4e12b3834"
+  sha256 "cf9ce8ed99cdf19471f93e20b267a23017d1ee61a18361c22a1c11c4a045576d"
   license ""
   head "https://github.com/darmiel/nudge.git", branch: "main"
 
